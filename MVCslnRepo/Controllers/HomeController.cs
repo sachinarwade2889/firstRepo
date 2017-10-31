dad304fc202519ab55a1f90872ab734334435437
sachin arwade <sachin.arwade2889@gmail.com>
2017-10-31 00:20:01 -0700

    Added branchnewtask and changed HomeController

--- MVCslnRepo/Controllers/HomeController.cs
+++ MVCslnRepo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MVCslnRepo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. New branch!";
 
             return View();
         }
